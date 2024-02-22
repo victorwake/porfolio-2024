aa5b012710b577c79005e55331a17763341d0c6c
Victor Pinto <victorpintowake@gmail.com>
2024-02-21 22:46:39 -0300

    games work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
 import iglGranos from '../assets/img/work/iglGranos.jpg'
+import game from '../assets/img/work/game.jpg'
+
 const Work = () => {
   const [t, i18n] = useTranslation("global");
   const handleMouseOver = (event) => {
@@ -55,15 +57,15 @@ const Work = () => {
             <li className="cards_item">
               <div className="card">
                 <div className="card_image">
-                  <img className="img-work" src={iglGranos}></img>
+                  <img className="img-work" src={game}></img>
                 </div>
                 <div className="card_content">
-                  <h2 className="card_title">IGL Granos S.A.</h2>
+                  <h2 className="card_title">PI soy Henry</h2>
                   <p className="card_text">
-                    Pagina para empresa exportador de materias primas agrícolas, centrándose en productos orgánicos.
+                    Proyecto individual para el curso fullStack de soyHenry.
                   </p>
                   <button className="btn card_btn">
-                  <a  href="https://victorwake.github.io/igl-granos/" target="_blank">View Website</a>
+                  <a  href="https://pi-videogames-pza6s3qw0-victorwake.vercel.app/" target="_blank">View Website</a>
                   </button>
                 </div>
               </div>
