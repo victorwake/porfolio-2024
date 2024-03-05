0c7ed3a2b8bcba7da766c74a7f7b5e7c00bb6932
Victor Pinto <victorpintowake@gmail.com>
2024-03-04 22:22:02 -0300

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import { useTranslation } from 'react-i18next';
+import whatsapp from '../assets/img/whatsapp.png'
+import gmail from '../assets/img/gmail.png'
 const Contact = () => {
     const [t, i18n] = useTranslation('global');
   const handleMouseOver = (event) => {
@@ -27,6 +29,10 @@ const Contact = () => {
                 </span>
                 ))}
                 </h2>
+                <div className="socialLinks">
+                <a href="https://api.whatsapp.com/send/?phone=541131158866" target="_blank"><img className="imgSkills" src={whatsapp} alt="whatsapp" width={40}></img></a>
+                <a href="mailto:victorpintowake@gmail.com" target="_blank"><img className="imgSkills" src={gmail} alt="gmail" width={40}></img></a>
+                </div>
       </section>
        </>
     )
